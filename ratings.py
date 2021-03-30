90b3bcedd624698ff9182169cf8198fe20afec33
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-03-30 01:48:19 +0000

    Added doctstrings to the functions

--- ratings.py
+++ ratings.py
@@ -1,11 +1,11 @@
 """Restaurant rating lister."""
 
 def restaurant_ratings_scores(filename):
-
+    """ Read a file, store the contents in a dictionary and return the dictionary."""
+    
     restaurant_data = open(filename)
-
     restaurant_dict = {}
-
+ 
     for line in restaurant_data:
         restaurant_name = line.rstrip().split(':')[0]
         rating = line.rstrip().split(':')[1]
@@ -15,12 +15,14 @@ def restaurant_ratings_scores(filename):
 
 
 def print_ratings(scores_dict):
+    """ Print the contents of the dictionary in a sorted order."""
 
-  for restaurant_name, rating in sorted(scores_dict.items()):
+    for restaurant_name, rating in sorted(scores_dict.items()):
       print(f"{restaurant_name} is rated at {rating}")
 
 
 def user_input(scores_dict):
+    """ Add a user entered resturant name and rating to the exixting dictionary """
 
     user_restaurant_name = input("Enter the restaurant name: ")
     user_rating = int(input("Enter the restaurant's rating: "))
